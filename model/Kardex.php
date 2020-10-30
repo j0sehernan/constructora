f9ec5b114094e13f5e54dd356a8675624ec1b07f
j0sehernan <joseh.quispec@gmail.com>
2020-10-30 07:48:44 -0500

    test

--- model/Kardex.php
+++ model/Kardex.php
@@ -10,10 +10,12 @@ class Kardex
             "k.unidad_medida_id, um.nombre as unidad_medida," .
             "p.nombre as producto," .
             "if(k.fecha_ingreso='0000-00-00', '', date_format(k.fecha_ingreso, '%d/%m/%Y')) as fecha_ingreso," .
-            "ifnull(if(k.fecha_vencimiento='0000-00-00', '', date_format(k.fecha_vencimiento, '%d/%m/%Y')), '')as fecha_vencimiento " .
+            "ifnull(if(k.fecha_vencimiento='0000-00-00', '', date_format(k.fecha_vencimiento, '%d/%m/%Y')), '')as fecha_vencimiento, " .
+            "ifnull(km.guia_remision, '') as guia_remision " .
             "from kardex k " .
             "inner join producto p on k.producto_id = p.id " .
             "inner join unidad_medida um on k.unidad_medida_id = um.codigo " .
+            "left join kardex_movimiento km on k.id = km.kardex_id and km.id = (SELECT max(km2.id) FROM kardex_movimiento km2 where km2.kardex_id = k.id) " .
             "where k.almacen_id = $almacen_id " .
             "and del = $del");
         return $result;
