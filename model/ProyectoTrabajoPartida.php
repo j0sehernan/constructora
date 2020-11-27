3641fc2eaa45f961eb84ead5aaf5f76b0be415f2
j0sehernan <joseh.quispec@gmail.com>
2020-11-27 16:18:26 -0500

    commit with erros

--- model/ProyectoTrabajoPartida.php
+++ model/ProyectoTrabajoPartida.php
@@ -60,7 +60,7 @@ class ProyectoTrabajoPartida
         return $result;
     }
 
-    //This is a normal test 
+    //This is a normal test change by error
     function listSonsByProyectoTrabajoPartida($proyecto_trabajo_partida_id)
     {
         $db = new DB();
