7deff534bc1dc5dc430bb406a1c7c3d4890db1a8
j0sehernan <joseh.quispec@gmail.com>
2020-12-23 22:58:44 -0500

    JH

--- controller/KardexMovimiento_C.php
+++ controller/KardexMovimiento_C.php
@@ -21,15 +21,18 @@ switch ($object->action) {
         $report = array();
         $total_cantidad_entrada = 0;
         $total_cantidad_salida = 0;
+        $total_sub_total = 0;
+        $stock_actual = 0;
 
         for ($i = 0; $i < count($result); ++$i) {
             $tipo_movimiento = $result[$i]["tipo_movimiento"];
+            $precio = $result[$i]["precio"];
             $documento = "";
             $orden_compra_entrada__partida_salida = "";
             $proveedor_contratista_almacen = "";
             $cantidad_entrada = 0;
             $cantidad_salida = 0;
-            $stock_actual = 0;
+            $sub_total = 0;
 
             switch ($tipo_movimiento) {
                 case "D":
@@ -38,40 +41,64 @@ switch ($object->action) {
                 case "I_PRODUCTO":
                     $tipo_movimiento_text = "INGRESO POR PRODUCTO";
                     $documento = $result[$i]["comprobante_pago_codigo"];
+                    $cantidad_entrada = $result[$i]["cantidad"];
+                    $stock_actual += $cantidad_entrada;
                     break;
                 case "I_ALMACEN":
                     $tipo_movimiento_text = "INGRESO POR CAMBIO DE ALMACEN";
                     $proveedor_contratista_almacen = $result[$i]["almacen_origen"];
+                    $cantidad_entrada = $result[$i]["cantidad"];
+                    $stock_actual += $cantidad_entrada;
                     break;
                 case "S_ALMACEN":
                     $tipo_movimiento_text = "SALIDA POR CAMBIO DE ALMACEN";
                     $proveedor_contratista_almacen = $result[$i]["almacen_salida"];
+                    $cantidad_salida = $result[$i]["cantidad_salida"];
+                    $stock_actual -= $cantidad_salida;
                     break;
                 case "I_CONVERT_NEW":
                     $tipo_movimiento_text = "INGRESO POR CONVERSIÓN DE UM";
+                    $cantidad_entrada = $result[$i]["cantidad"];
+                    $stock_actual += $cantidad_entrada;
                     break;
                 case "I_OC":
                     $tipo_movimiento_text = "INGRESO POR ORDEN DE COMPRA";
                     $documento = $result[$i]["guia_remision"];
                     $orden_compra_entrada__partida_salida = $result[$i]["orden_compra"];
                     $proveedor_contratista_almacen = $result[$i]["proveedor_ingreso"];
+                    $cantidad_entrada = $result[$i]["cantidad"];
+                    $stock_actual += $cantidad_entrada;
                     break;
                 case "S_PARTIDA":
                     $tipo_movimiento_text = "SALIDA POR ASIGNACIÓN A PARTIDA";
                     $documento = $result[$i]["numero_vale"];
                     $orden_compra_entrada__partida_salida = $result[$i]["proyecto_trabajo_partida_salida"];
                     $proveedor_contratista_almacen = $result[$i]["contratista_salida"];
+                    $cantidad_salida = $result[$i]["cantidad_salida"];
+                    $stock_actual -= $cantidad_salida;
                     break;
                 case "I_CONVERT_UPDATE":
                     $tipo_movimiento_text = "ACTUALIZACIÓN DE CANTIDAD POR CONVERSIÓN DE UM";
+                    $cantidad_entrada = $result[$i]["cantidad"];
+                    $stock_actual += $cantidad_entrada;
                     break;
                 case "I_CONVERT_UPDATE_FINISH":
                     $tipo_movimiento_text = "ACTUALIZACIÓN Y FINALIZACIÓN DE CANTIDAD POR CONVERSIÓN DE UM";
+                    $cantidad_entrada = $result[$i]["cantidad"];
+                    $stock_actual += $cantidad_entrada;
                     break;
                 case "I_PARTIDA":
                     $tipo_movimiento_text = "REINGRESO DESDE PARTIDA";
+                    $cantidad_entrada = $result[$i]["cantidad"];
+                    $stock_actual += $cantidad_entrada;
                     break;
             }
+
+            $sub_total = $cantidad_entrada * $precio;
+
+            $total_cantidad_entrada += $cantidad_entrada;
+            $total_cantidad_salida += $cantidad_salida;
+            $total_sub_total += $sub_total;
 
             $objectReport = array(
                 "index" => $i + 1,
@@ -81,11 +108,11 @@ switch ($object->action) {
                 "documento" => $documento,
                 "oc_p" => $orden_compra_entrada__partida_salida,
                 "p_c_a" => $proveedor_contratista_almacen,
-                "cantidad_entrada" => $cantidad_entrada,
-                "cantidad_salida" => $cantidad_salida,
-                "stock_actual" => $stock_actual,
-                "precio" => $result[$i]["precio"],
-                "sub_total" => ""
+                "cantidad_entrada" => number_format($cantidad_entrada, 2, '.', ','),
+                "cantidad_salida" => number_format($cantidad_salida, 2, '.', ','),
+                "stock_actual" => number_format($stock_actual, 2, '.', ','),
+                "precio" => number_format($precio, 2, '.', ','),
+                "sub_total" => number_format($sub_total, 2, '.', ',')
             );
 
             array_push($report, $objectReport);
@@ -103,7 +130,7 @@ switch ($object->action) {
             "cantidad_salida" => number_format($total_cantidad_salida, 2, '.', ','),
             "stock_actual" => number_format(($total_cantidad_entrada - $total_cantidad_salida), 2, '.', ','),
             "precio" => "",
-            "sub_total" => ""
+            "sub_total" => number_format($total_sub_total, 2, '.', ',')
         );
 
         array_push($report, $objectReport);
